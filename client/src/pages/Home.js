191ff7f76d3034841203b044975b78cf8c742acf
AznRaven <74807576+AznRaven@users.noreply.github.com>
2023-10-04 10:59:46 -0500

    installed proxy

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
+import axios from "axios"; // Import Axios
 
 export default function Home() {
   const [workouts, setWorkouts] = useState(null);
   
   useEffect(() => {
-    const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
-      const json = await response.json()
+    // const fetchWorkouts = async () => {
+    //   const response = await fetch('/api/workouts')
+    //   const json = await response.json()
 
-      if (response.ok) {
-        setWorkouts(json)
+    //   if (response.ok) {
+    //     setWorkouts(json)
+    //   }
+    // }
+    const fetchWorkouts = async () => {
+      try {
+        const response = await axios.get("/api/workouts"); // Use Axios to make the GET request
+        if (response.status === 200) {
+          setWorkouts(response.data); // Assuming the JSON response is in response.data
+        }
+      } catch (error) {
+        console.error("Error:", error);
       }
-    }
+    };
 
     fetchWorkouts();
   }, []);
